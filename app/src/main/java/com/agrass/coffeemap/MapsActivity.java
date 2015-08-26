a42d8ad3a03743af7f1af3393f7a510f95c0af35
Agrass <Agrass_3840@gmail.com>
2015-08-26 12:50:25 +0300

    OSM ready!

--- app/src/main/java/com/agrass/coffeemap/MapsActivity.java
+++ app/src/main/java/com/agrass/coffeemap/MapsActivity.java
@@ -1,28 +1,33 @@
 package com.agrass.coffeemap;
 
-import android.support.v4.app.FragmentActivity;
+import android.app.Activity;
 import android.os.Bundle;
-
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import org.osmdroid.views.MapController;
+import org.osmdroid.views.MapView;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends Activity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
-
+    private MapView myOpenMapView;
+    private MapController myMapController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        setUpMapIfNeeded();
+//        setUpMapIfNeeded();
+        myOpenMapView = (MapView)findViewById(R.id.openmapview);
+        myOpenMapView.setBuiltInZoomControls(true);
+        myMapController = (MapController) myOpenMapView.getController();
+        myMapController.setZoom(4);
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        setUpMapIfNeeded();
+//        setUpMapIfNeeded();
     }
 
     /**
@@ -40,18 +45,18 @@ public class MapsActivity extends FragmentActivity {
      * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
      * method in {@link #onResume()} to guarantee that it will be called.
      */
-    private void setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
-            // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                setUpMap();
-            }
-        }
-    }
+//    private void setUpMapIfNeeded() {
+//        // Do a null check to confirm that we have not already instantiated the map.
+//        if (mMap == null) {
+//            // Try to obtain the map from the SupportMapFragment.
+//            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+//                    .getMap();
+//            // Check if we were successful in obtaining the map.
+//            if (mMap != null) {
+//                setUpMap();
+//            }
+//        }
+//    }
 
     /**
      * This is where we can add markers or lines, add listeners or move the camera. In this case, we
