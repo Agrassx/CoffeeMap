c4bbc5e3a10140550894c3cc30b4b52f4afe804a
Agrass <Agrass_3840@gmail.com>
2015-09-26 13:23:04 +0300

    bbox in url

--- app/src/main/java/com/agrass/coffeemap/CoffeeAPI.java
+++ app/src/main/java/com/agrass/coffeemap/CoffeeAPI.java
@@ -13,21 +13,36 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.osmdroid.util.BoundingBoxE6;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.overlay.OverlayItem;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 
 public class CoffeeAPI {
 
     private JSONArray JsonCoffeeArray;
     private ArrayList CoffeeList = new ArrayList<OverlayItem>();
+    private String urlMain = "http://78.47.49.234:9000/api/points?";
 
 
-    public void setCoffeeOverlay(Context context, String url) {
+    public void setCoffeeOverlay(Context context, BoundingBoxE6 boundingBox) {
+
+        Double N = round(boundingBox.getLatNorthE6()/1E6, 2);
+        Double S = round(boundingBox.getLatSouthE6()/1E6, 2);
+        Double W = round(boundingBox.getLonWestE6()/1E6, 2);
+        Double E = round(boundingBox.getLonEastE6()/1E6, 2);
+        Log.wtf("N",N.toString());
+        String url1 = urlMain + "s="+S.toString()+"&"+"n="+N.toString()+"&"+"w="+W.toString()+"&"+"e="+E.toString();
+        String url = "http://78.47.49.234:9000/api/points?s=55.75&n=55.74&w=37.60&e=37.66";
+        Log.wtf("URL", url);
+        Log.wtf("URL1", url1);
 
         RequestQueue queue = Volley.newRequestQueue(context);
-        JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.GET, url, new Response.Listener<JSONObject>() {
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.GET, url1, new Response.Listener<JSONObject>() {
 
             @Override
             public void onResponse(JSONObject response) {
@@ -68,9 +83,14 @@ public class CoffeeAPI {
         return JsonCoffeeArray.getJSONObject(index).getInt("id");
     }
 
-    public int length(){
-        return JsonCoffeeArray.length();
+    private static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
     }
+
     public ArrayList<OverlayItem> getOverlay() {
             return CoffeeList;
     }
--- app/src/main/java/com/agrass/coffeemap/MapsActivity.java
+++ app/src/main/java/com/agrass/coffeemap/MapsActivity.java
@@ -6,25 +6,14 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.osmdroid.ResourceProxy;
-import org.osmdroid.api.IGeoPoint;
 import org.osmdroid.events.DelayedMapListener;
 import org.osmdroid.events.MapListener;
 import org.osmdroid.events.ScrollEvent;
 import org.osmdroid.events.ZoomEvent;
 import org.osmdroid.tileprovider.MapTileProviderBasic;
 import org.osmdroid.tileprovider.tilesource.ITileSource;
+import org.osmdroid.util.BoundingBoxE6;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.ItemizedIconOverlay;
@@ -51,6 +40,8 @@ public class MapsActivity extends Activity {
     private static final String PREFS_NAME = "MyPrefsFile";
     private ResourceProxy mResourceProxy;
     private GeoPoint Moscow = new GeoPoint(55751556, 37624482);
+    private GeoPoint NE;
+    private GeoPoint SW;
     private static final String	baseUrls[] = { "http://a.tiles.maps.sputnik.ru/tiles/kmt2/",
                                                "http://b.tiles.maps.sputnik.ru/tiles/kmt2/",
                                                "http://c.tiles.maps.sputnik.ru/tiles/kmt2/",
@@ -84,35 +75,24 @@ public class MapsActivity extends Activity {
 
         SputnikMap.setTileSource(tileSource);
 
-        final String url = "http://78.47.49.234:9000/api/points?s=55.75&n=55.74&w=37.60&e=37.66";
         final CoffeeAPI coffeeAPI = new CoffeeAPI();
         final ArrayList CoffeeMarkers = new ArrayList<OverlayItem>();
 
         SputnikMap.setMapListener(new DelayedMapListener(new MapListener() {
             public boolean onZoom(final ZoomEvent e) {
                 Log.wtf("zoom", "it was zoomed");
-                Integer Zoom = SputnikMap.getZoomLevel();
-                Log.wtf("zoom", "Zoom is " + Zoom.toString());
-                Integer N = SputnikMap.getBoundingBox().getLatNorthE6();
-                Integer E = SputnikMap.getBoundingBox().getLonEastE6();
-                Integer S = SputnikMap.getBoundingBox().getLatSouthE6();
-                Integer W = SputnikMap.getBoundingBox().getLonWestE6();
-                Log.wtf("N", "N is " + N.toString());
-                Log.wtf("E", "E is " + E.toString());
-                Log.wtf("S", "S is " + S.toString());
-                Log.wtf("W", "W is " + W.toString());
+                coffeeAPI.setCoffeeOverlay(SputnikMap.getContext(), SputnikMap.getBoundingBox());
+                ItemizedIconOverlay<OverlayItem> CoffeeMarkersItemizedIconOverlay
+                        = new ItemizedIconOverlay<OverlayItem>(
+                        SputnikMap.getContext(), coffeeAPI.getOverlay(), null);
+                SputnikMap.getOverlays().add(CoffeeMarkersItemizedIconOverlay);
+
                 return true;
             }
 
             public boolean onScroll(final ScrollEvent e) {
                 Log.wtf("scroll", "it was scrolled");
-
-                coffeeAPI.setCoffeeOverlay(SputnikMap.getContext(), url);
-//                CoffeeMarkers.add(coffeeAPI.getOverlay());
-
-                Boolean s = CoffeeMarkers.isEmpty();
-                Log.wtf("CoffeeMarkers is Empty?", s.toString());
-
+                coffeeAPI.setCoffeeOverlay(SputnikMap.getContext(), SputnikMap.getBoundingBox());
                 ItemizedIconOverlay<OverlayItem> CoffeeMarkersItemizedIconOverlay
                         = new ItemizedIconOverlay<OverlayItem>(
                         SputnikMap.getContext(), coffeeAPI.getOverlay(), null);
